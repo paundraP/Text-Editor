c5fea0ed0f7de085ede75eb23832a155a64c5fcf
nocan0 <d1r4m15a@gmail.com>
2025-04-15 22:02:10 +0700

    insertChar update

--- TextEditor.h
+++ TextEditor.h
@@ -20,8 +20,38 @@ private:
 public:
   TextEditor() : head(nullptr), tail(nullptr), length(0) {}
   void insertChar(char c);
+  
   void deleteChar();
   void undo();
   void redo();
   void printText();
 };
+void TextEditor::insertChar(char c) {
+  // Buat node baru
+  CharNode* newNode = new CharNode(c);
+  
+  // Jika teks masih kosong
+  if (head == nullptr) {
+      head = tail = newNode;
+  } else {
+      // Sisipkan di akhir (tail)
+      tail->next = newNode;
+      tail = newNode;
+  }
+  
+  // Tambahkan action ke undo stack
+  Action action;
+  action.type = DELETE;  // Untuk undo insert adalah delete
+  action.character = c;
+  action.posisiton = length;  // Posisi karakter yang dimasukkan
+  
+  undoStack.push(action);
+  
+  // Reset redo stack karena ada operasi baru
+  while (!redoStack.isEmpty()) {
+      redoStack.pop();
+  }
+  
+  // Update panjang teks
+  length++;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include "TextEditor.h"
 using namespace std;
 
-int main() { cout << "hello world" << endl; }
+int main() { 
+    cout << "hello world" << endl; 
+    TextEditor x;
+    x.insertChar('S');
+    x.insertChar('k');
+    while (x->next != NULL);
+    return 0;
+}
